690bdb1ee4e8ba5da936e302eea2249b689cdbf1
sur-pavel <sur-pavel@mail.ru>
2021-08-05 11:57:58 +0300

    ad mystem

---
+++ IrbisSearchEngine/MystemAnalysis.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace IrbisSearchEngine
+{
+    internal class MystemAnalysis
+    {
+        /// <summary>
+        /// Грамматический разбор слова
+        /// </summary>
+        [JsonProperty("gr")]
+        public string Grammeme { get; set; }
+
+        /// <summary>
+        /// Обсценное?
+        /// </summary>
+        public bool IsObscene
+        {
+            get { return Split.Contains("обсц"); }
+        }
+
+        /// <summary>
+        /// Основная форма слова
+        /// </summary>
+        [JsonProperty("lex")]
+        public string Lexeme { get; set; }
+
+        /// <summary>
+        /// Часть речи.
+        /// </summary>
+        public string PartOfSpeech
+        {
+            get { return Split[0].Trim('='); }
+        }
+
+        /// <summary>
+        /// Относительный вес
+        /// </summary>
+        [JsonProperty("wt")]
+        public double Weight { get; set; }
+
+        private string[] Split
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(Grammeme))
+                    return new[] { String.Empty };
+                return Grammeme.Split(',');
+            }
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return string.Format
+                (
+                    "{0}, {1}",
+                    Lexeme,
+                    PartOfSpeech
+                );
+        }
+    }
+}
---
+++ IrbisSearchEngine/MystemResult.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IrbisSearchEngine
+{
+    internal class MystemResult
+    {
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        public MystemAnalysis[] Analysis { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append(Text);
+            foreach (MystemAnalysis analysis in Analysis)
+            {
+                result.Append("; ");
+                result.Append(analysis);
+            }
+            return result.ToString();
+        }
+    }
+}
