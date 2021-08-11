30411498eba45e93fddad678dbcf3ffd22898f37
sur-pavel <sur-pavel@mail.ru>
2021-08-11 10:19:35 +0300

    next commit

--- IrbisSearchEngine/MystemAnalysis.cs
+++ IrbisSearchEngine/MystemAnalysis.cs
@@ -10,37 +10,22 @@ namespace IrbisSearchEngine
 {
     internal class MystemAnalysis
     {
-        /// <summary>
-        /// Грамматический разбор слова
-        /// </summary>
         [JsonProperty("gr")]
         public string Grammeme { get; set; }
 
-        /// <summary>
-        /// Обсценное?
-        /// </summary>
         public bool IsObscene
         {
             get { return Split.Contains("обсц"); }
         }
 
-        /// <summary>
-        /// Основная форма слова
-        /// </summary>
         [JsonProperty("lex")]
         public string Lexeme { get; set; }
 
-        /// <summary>
-        /// Часть речи.
-        /// </summary>
         public string PartOfSpeech
         {
             get { return Split[0].Trim('='); }
         }
 
-        /// <summary>
-        /// Относительный вес
-        /// </summary>
         [JsonProperty("wt")]
         public double Weight { get; set; }
 
@@ -54,7 +39,6 @@ namespace IrbisSearchEngine
             }
         }
 
-        /// <inheritdoc />
         public override string ToString()
         {
             return string.Format
